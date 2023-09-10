1bc6bf24e2ccdd56f75c13aaedb1081a69b00d31
GennadiyPob <137322270+GennadiyPob@users.noreply.github.com>
2023-09-10 15:05:38 +0300

    Update

---
+++ second_package/script_two.py
@@ -0,0 +1,13 @@
+''' Імпорт пакетів '''
+
+'''Перший варіант'''
+
+import first_package.script_one
+# виклик функції
+first_package.script_one.get_square(8)
+
+'''Другий варіант'''
+
+# from first_package import script_one
+# # виклик функції
+# script_one.get_square(10)
